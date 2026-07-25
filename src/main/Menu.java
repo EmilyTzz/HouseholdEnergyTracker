33ce05d0917d2b1dd3661420b6c5b2b178b42e11
EmilyT2011 <khanhtrinh201107@gmail.com>
2026-07-24 21:08:22 -0600

    Added user input for monthly energy consumption and added main menu options for the application

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -1,4 +1,73 @@
 package main;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Menu {
+
+    private static ArrayList<String> validMonths = new ArrayList<>(); // stores all the valid months
+
+    private static ArrayList<String> menuOptions = new ArrayList<>(); // stores all the main menu options
+
+    static{
+        validMonths.addAll(Arrays.asList("JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY",
+                "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"));
+        menuOptions.addAll(Arrays.asList("Enter This Month's Energy Consumption", "View Consumption Summary",
+                "Save", "Load", "Quit"));
+    }
+
+    public static void mainMenu(){
+        System.out.println("--------------Household Energy Tracker--------------");
+
+        consumptionInputMenu();
+    }
+
+    public static void consumptionInputMenu(){
+        System.out.println("--------------Your Energy Consumption This Month--------------");
+        String month;
+        double electricity;
+        double naturalGas;
+        // Month Input
+        while (true){
+            System.out.print("Month: ");
+            Scanner scanner = new Scanner(System.in);
+            month = scanner.next().toUpperCase();
+            if (validMonths.contains(month)) {
+                break;
+            }
+        }
+        System.out.println(month);
+        // Electricity Input
+        while (true){
+            try{
+                System.out.println("Electricity Used (kW): ");
+                Scanner scanner = new Scanner(System.in);
+                electricity = scanner.nextDouble();
+                if (electricity >= 0){
+                    break;
+                }
+            }catch (InputMismatchException e){
+                System.out.println("Please enter a valid amount");
+            }
+        }
+        System.out.println(electricity);
+        // Natural Gas Input
+        while (true){
+            try{
+                System.out.println("Natural Gas Used (GJ): ");
+                Scanner scanner = new Scanner(System.in);
+                naturalGas = scanner.nextDouble();
+                if (naturalGas >= 0){
+                    break;
+                }
+            }catch (InputMismatchException e){
+                System.out.println("Please enter a valid amount");
+            }
+        }
+        System.out.println(naturalGas);
+    }
+
 }
