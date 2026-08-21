0da2182e4387a3fa76ff992d4e25377ab905a749
EmilyT2011 <khanhtrinh201107@gmail.com>
2026-08-20 22:33:09 -0600

    Added the 3 main tabs to the TabPane (Add Monthly Usage, Edit Prices, and View Overview)

--- src/main/java/main/MainController.java
+++ src/main/java/main/MainController.java
@@ -1,11 +1,31 @@
 package main;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 
 public class MainController {
 
     @FXML
+    private Tab editPricesTab;
+
+    @FXML
+    private Tab enterMonthlyUsageTab;
+
+    @FXML
+    private Tab viewOverview = new Tab("View Overview");;
+
+    @FXML
     public void initialize() {
-        System.out.println("Worked");
+        TabPane tabPane = new TabPane();
+        enterMonthlyUsageTab.setClosable(false); // Don't allow user to close the tab
+        tabPane.getTabs().add(enterMonthlyUsageTab); // Add tab to the tabPane
+        editPricesTab.setClosable(false);
+        tabPane.getTabs().add(editPricesTab);
+        viewOverview.setClosable(false);
+        tabPane.getTabs().add(viewOverview);
     }
+
+
 }
+
